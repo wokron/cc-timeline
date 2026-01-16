b6366ccc53e86f3207aa966fddb57f97fda455c5
wokron <yi1tang.yang@gmail.com>
2026-01-16 15:59:36 +0800

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// Activate debug mode
+    #[clap(short, long)]
+    debug: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    if args.debug {
+        println!("Debug mode is on");
+    } else {
+        println!("Debug mode is off");
+    }
 }
